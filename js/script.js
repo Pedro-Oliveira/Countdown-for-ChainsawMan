6eea86707cfb7a7fe412a197c74ab9cbb3b71eb7
Pedro Henrique Pinto O. Santos <76534553+Pedro-Oliveira@users.noreply.github.com>
2021-04-19 23:28:26 -0300

    reponsive
    I ajusted the responsive of the site.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const dias = document.getElementById('days');
+const horas = document.getElementById('hours');
+const minu = document.getElementById('minutes');
+const segun = document.getElementById('seconds');
+const theday = ('27 july 2021')
