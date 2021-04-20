0497730fe1fb08d5f121440a2967b4c3735c6347
Pedro Henrique Pinto O. Santos <76534553+Pedro-Oliveira@users.noreply.github.com>
2021-04-20 20:37:16 -0300

    countdownscript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-const dias = document.getElementById('days');
-const horas = document.getElementById('hours');
-const minu = document.getElementById('minutes');
-const segun = document.getElementById('seconds');
-const theday = ('27 july 2021')
+const dias = document.getElementById('daysEl');
+const horas = document.getElementById('hoursEl');
+const minu = document.getElementById('minutesEl');
+const segun = document.getElementById('secondsEl');
+const theday = "27 July 2021";
+function contador(){
+    const thedaydate = new Date(theday);
+    const rightnow = new Date();
+    const totalseconds = (thedaydate - rightnow) / 1000;
+    const daysEl = Math.floor(totalseconds / 3600 / 24);
+    const hoursEl = Math.floor(totalseconds / 3600) % 24;
+    const minutesEl = Math.floor(totalseconds / 60) % 60;
+    const secondsEl = Math.floor(totalseconds) % 60;
+
+    dias.innerHTML = daysEl;
+    horas.innerHTML = formatTime(hoursEl);
+    minu.innerHTML = formatTime(minutesEl);
+    segun.innerHTML = formatTime(secondsEl); 
+}
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+contador();
+setInterval(contador, 1000);
